eefc6d62dffe04d8ee21ba56ddc98f9805090a15
aviel1230 <avielh110@gmail.com>
2022-09-07 20:05:34 +0300

    Update aviel.py

--- aviel.py
+++ aviel.py
@@ -2,4 +2,18 @@ import turtle
 print("hello world")
 print("hello world")
 for k in range(1,10):
-    print(k)
+    print("cat")
+
+a = 330
+b = 200
+if b > a:
+  print("b is greater than a")
+else:
+  print("a is greater than b")
+c = 201
+d = 400
+if c != d:
+  print("aviel the king")
+else:
+  print("liron the queen")
+  
